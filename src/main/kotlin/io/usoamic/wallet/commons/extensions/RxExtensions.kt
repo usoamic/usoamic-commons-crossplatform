c9f09e22d3d762730cd2f1da0e3daabe503f9400
primys <a2uso@tuta.io>
2021-01-17 14:25:37 -0800

    Remove several extensions

--- src/main/kotlin/io/usoamic/wallet/commons/extensions/RxExtensions.kt
+++ src/main/kotlin/io/usoamic/wallet/commons/extensions/RxExtensions.kt
@@ -2,16 +2,12 @@ package io.usoamic.wallet.commons.extensions
 
 import io.reactivex.Completable
 import io.reactivex.Single
-import io.reactivex.rxjavafx.schedulers.JavaFxScheduler
+
 import io.reactivex.schedulers.Schedulers
 import io.usoamic.wallet.commons.Config
 import java.util.concurrent.TimeUnit
 
-fun <T> Single<T>.observeOnMain(): Single<T> = observeOn(JavaFxScheduler.platform())
-
 fun <T> Single<T>.subscribeOnIo(): Single<T> = subscribeOn(Schedulers.io())
-
-fun <T> Single<T>.addSchedulers(): Single<T> = subscribeOnIo().observeOnMain()
 
 fun <T> Single<T>.addDebugDelay(): Single<T> {
     if(Config.DEBUG) {
