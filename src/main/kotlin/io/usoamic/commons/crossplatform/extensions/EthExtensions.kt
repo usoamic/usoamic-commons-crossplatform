607d32ba2743446bc12576a90893251dfe00a0d0
primys <primys@usoamic.io>
2021-05-02 03:42:37 -0700

    Add toCleanedHexPrefixString

--- src/main/kotlin/io/usoamic/commons/crossplatform/extensions/EthExtensions.kt
+++ src/main/kotlin/io/usoamic/commons/crossplatform/extensions/EthExtensions.kt
@@ -1,5 +1,10 @@
 package io.usoamic.commons.crossplatform.extensions
 
 import org.web3j.crypto.Credentials
+import org.web3j.utils.Numeric
 
 val Credentials.privateKey: String get() = ecKeyPair.privateKey.toString(16)
+
+fun String.toCleanedHexPrefixString(): String {
+    return Numeric.cleanHexPrefix(this)
+}
