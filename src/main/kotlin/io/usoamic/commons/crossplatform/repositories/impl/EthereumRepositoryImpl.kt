e4a6bfe04264fda9c28716bd7abefbffe6ce46df
primys <primys@usoamic.io>
2021-12-06 00:59:37 +0300

    Add getBalance by address for EthereumRepository

--- src/main/kotlin/io/usoamic/commons/crossplatform/repositories/impl/EthereumRepositoryImpl.kt
+++ src/main/kotlin/io/usoamic/commons/crossplatform/repositories/impl/EthereumRepositoryImpl.kt
@@ -4,6 +4,7 @@ import io.reactivex.Single
 import io.usoamic.commons.crossplatform.extensions.addDebugDelay
 import io.usoamic.commons.crossplatform.extensions.privateKey
 import io.usoamic.commons.crossplatform.mappers.entity.toEntity
+import io.usoamic.commons.crossplatform.mappers.entity.toEther
 import io.usoamic.commons.crossplatform.models.repository.add.AddAccountEntity
 import io.usoamic.commons.crossplatform.models.repository.ethereum.AccountCredentialsEntity
 import io.usoamic.commons.crossplatform.models.repository.withdraw.WithdrawRequest
@@ -20,6 +21,40 @@ import javax.inject.Inject
 class EthereumRepositoryImpl @Inject constructor(
     private val usoamic: Usoamic
 ) : EthereumRepository {
+    override val ethBalance: Single<BigDecimal>
+        get() {
+            return Single.fromCallable {
+                usoamic.getConvertedBalance()
+            }
+                .addDebugDelay()
+        }
+
+    override val ethHeight: Single<BigInteger>
+        get() {
+            return Single.fromCallable {
+                usoamic.getEthHeight()
+            }
+                .addDebugDelay()
+        }
+
+    override fun getBalance(address: String): Single<BigDecimal> {
+        return Single.fromCallable {
+            usoamic.getEthBalance(address).toEther()
+        }.addDebugDelay()
+    }
+
+    override fun withdraw(data: WithdrawRequest): Single<String> {
+        return Single.fromCallable {
+            val value = Convert.toWei(data.value, Convert.Unit.ETHER)
+            usoamic.transferEth(
+                password = data.password,
+                to = data.to,
+                value = value.toBigInteger(),
+                txSpeed = data.txSpeed
+            )
+        }.addDebugDelay()
+    }
+
     override fun addAccount(privateKey: String, password: String): Single<AddAccountEntity> {
         return Single.fromCallable {
             AddAccountEntity(
@@ -45,33 +80,5 @@ class EthereumRepositoryImpl @Inject constructor(
             credentials.toEntity()
         }
             .addDebugDelay()
-    }
-
-    override val ethBalance: Single<BigDecimal>
-        get() {
-            return Single.fromCallable {
-                usoamic.getConvertedBalance()
-            }
-                .addDebugDelay()
-        }
-
-    override val ethHeight: Single<BigInteger>
-        get() {
-            return Single.fromCallable {
-                usoamic.getEthHeight()
-            }
-                .addDebugDelay()
-        }
-
-    override fun withdraw(data: WithdrawRequest): Single<String> {
-        return Single.fromCallable {
-            val value = Convert.toWei(data.value, Convert.Unit.ETHER)
-            usoamic.transferEth(
-                password = data.password,
-                to = data.to,
-                value = value.toBigInteger(),
-                txSpeed = data.txSpeed
-            )
-        }.addDebugDelay()
     }
 }
